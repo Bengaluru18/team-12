9c7387d2c597e096d0153d9515648067e8b1b5f3
naman0105 <naman0105@gmail.com>
2018-07-07 18:40:21 +0530

    some basic structure

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
+var path = require('path')
 var admin = require("firebase-admin");
 var serviceAccount = require('./serviceAccountKey.json');
 
@@ -9,21 +10,51 @@ admin.initializeApp({
   databaseURL: "https://ssksolution-cb1ff.firebaseio.com",
 });
 
+var docter = require("./docter.js");
+var sw = require("./sw");
+var receptionist = require("./receptionist");
 
 var database = admin.database();
 
+router.get('/', function(req, res, next){
+  res.render('index', { })
+})
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  database.ref("users").set({
-    username: "hello",
-    email: "hell@gmail.com",
-    profile_picture : "someurl"
-  })
-  database.ref('/users').once('value').then(function(snapshot) {
-    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-    console.log(username);
-    res.render('index', { title: 'Express', username:username });
+
+router.post('/login', function(req, res, next) {
+  database.ref('/login').once('value').then(function(snapshot) {
+    var data = snapshot.val();
+    console.log(req.body);
+    console.log(snapshot.val());
+    var userid = req.body.username;
+    var password = req.body.password;
+    console.log(data.doc1);
+    snapshot.forEach(function(element){
+      if(userid == element.key){
+        console.log("user id match")
+        console.log(element.val().password)
+        console.log(password);
+          if(password == element.val().password){
+              console.log("password  match");
+              var type = element.val().type;
+              if(type == "docter"){
+                console.log("docter")
+                res.render('docter',{});
+              }
+            
+              if(type=="sw"){
+                console.log("sw")
+                res.render('sw',{})
+              }
+              
+              if(type=="receptionist"){
+                console.log("receptionist")
+                res.render('receptionist',{})
+              }
+
+          }
+        }
+    });
   });
 });
 
